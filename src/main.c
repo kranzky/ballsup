daa022e36082d21af44bfdb331284710514f6c9d
Jason Hutchens <jasonhutchens@gmail.com>
2020-02-02 12:01:38 +0800

    basic event loop

--- src/main.c
+++ src/main.c
@@ -3,11 +3,80 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+
 //==============================================================================
 
 //------------------------------------------------------------------------------
 
 int main(int argc, char **argv)
 {
+  bool quit = false;
+  int width = 1280;
+  int height = 720;
+  SDL_Window *window = NULL;
+  SDL_Renderer *screen = NULL;
+  
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0)
+    fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
+  else
+  {
+    SDL_JoystickEventState(SDL_ENABLE);
+    if (SDL_GameControllerAddMappingsFromFile("res/gamecontrollerdb.txt") < 0)
+      fprintf(stderr, "SDL_GameControllerAddMappingsFromFile: %s\n", SDL_GetError());
+    if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0"))
+      fprintf(stderr, "SDL_SetHint: SDL_HINT_RENDER_SCALE_QUALITY\n");
+    if (!SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1"))
+      fprintf(stderr, "SDL_SetHint: SDL_HINT_RENDER_VSYNC\n");
+    if (!SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1"))
+      fprintf(stderr, "SDL_SetHint: SDL_HINT_VIDEO_HIGHDPI_DISABLED\n");
+    if (!SDL_SetHint(SDL_HINT_VIDEO_MAC_FULLSCREEN_SPACES, "0"))
+      fprintf(stderr, "SDL_SetHint: SDL_HINT_VIDEO_MAC_FULLSCREEN_SPACES\n");
+    window = SDL_CreateWindow("Balls Up!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_INPUT_GRABBED);
+  }
+
+  if (!window)
+    fprintf(stderr, "SDL_CreateWindow: %s\n", SDL_GetError());
+  else
+  {
+    SDL_GetWindowSize(window, &width, &height);
+    screen = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  }
+
+  while (!quit)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0)
+    {
+      switch (event.type)
+      {
+        case SDL_QUIT:
+          quit = true;
+          break;
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym)
+          {
+            case SDLK_ESCAPE:
+              quit = true;
+              break;
+          }
+          break;
+      }
+    }
+  }
+
+  if (screen)
+  {
+    SDL_DestroyRenderer(screen);
+  }
+
+  if (window)
+  {
+    SDL_DestroyWindow(window);
+  }
+
+  SDL_Quit();
+
   return 0;
 }
